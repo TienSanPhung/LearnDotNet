0fd45f2a1ebf001fece6b5d663ad8062c06a2b4b
san san <pts.20.sn@gmail.com>
2025-05-04 09:53:57 +0700

    - finish: xong bài l17

---
+++ L17_18_ADONET/ADONETSample/ASample.Tests/ProgramTest.cs
@@ -0,0 +1,17 @@
+﻿using ASample;
+using JetBrains.Annotations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ASample.Tests;
+
+[TestClass]
+[TestSubject(typeof(Program))]
+public class ProgramTest
+{
+    [TestMethod]
+    public void DbConnectionTest()
+    {
+        Program.DbConnection();
+    }
+
+}
--- L17_18_ADONET/ADONETSample/ASample/Program.cs
+++ L17_18_ADONET/ADONETSample/ASample/Program.cs
@@ -1,9 +1,26 @@
-﻿namespace ASample;
+﻿using Microsoft.Data.SqlClient;
+
+namespace ASample;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        DbConnection();
+    }
+
+    public static void DbConnection()
+    {
+        using  var connect = new SqlConnection("Server=localhost,1433;Database=HR;User Id=SA;Password=Daicabavi1;TrustServerCertificate=true;");
+        connect.Open();
+         var cmd = connect.CreateCommand();
+         cmd.CommandText = "Select country_id, country_name from countries";
+        using  var reader = cmd.ExecuteReader();
+        { while (reader.Read())
+         {
+             Console.WriteLine($"ID: {reader["country_id"]}, Name: {reader["country_name"]}");
+         }
+         
+        }
     }
 }
