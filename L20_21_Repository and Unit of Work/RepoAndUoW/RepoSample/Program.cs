96c783fe3f11dc965b4e6ebd20c165f02c050c59
san san <pts.20.sn@gmail.com>
2025-05-11 13:24:57 +0700

    - finish: xong l20 hiểu sâu hơn về repository pattern và tính trừu tượng

--- L20_21_Repository and Unit of Work/RepoAndUoW/RepoSample/Program.cs
+++ L20_21_Repository and Unit of Work/RepoAndUoW/RepoSample/Program.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System.Text;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using RepoSample.Entity;
+using RepoSample.Repository;
+using RepoSample.Repository.InMemory;
+using RepoSample.Repository.SQLServer;
 
 namespace RepoSample;
 
@@ -6,10 +12,116 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.OutputEncoding = Encoding.UTF8;
         IConfiguration config = new ConfigurationBuilder()
             .AddJsonFile("appsettings.json", optional: true)
             .AddJsonFile("ConnectionStrings.json",optional:true).Build();
-        string connectionString = config.GetConnectionString("Shop");
+        string connectionString = config.GetConnectionString("Shop") ?? string.Empty;
+        if (connectionString.Length >0)
+        {
+            var conn = new SqlConnection(connectionString);
+            conn.Open();
+            var tran = conn.BeginTransaction();
+            
+            IProductRepository productRepository = new SqlProductRepository(conn, tran);
+            IOrderRepository orderRepository = new SqlOrderRepository(conn, tran);
+            
+            productRepository.DeleteAll();
+            orderRepository.DeleteAll();
+            
+            InsertProduct(productRepository);
+            QueryProduct(productRepository);
+            tran.Commit();
+            
+            CreateOder(conn);
+            QueryOder(orderRepository);
+        }
+        else
+        {
+            var inMemoryProduct = new InMemoryProductRepository();
+            var inMemoryOrder = new InMemoryProductRepository();
+            
+            InsertProduct(inMemoryProduct);
+            QueryProduct(inMemoryProduct);
+        }
+    }
+
+    private static void QueryOder(IOrderRepository orderRepository)
+    {
         
+    }
+
+    private static void CreateOder(SqlConnection conn)
+    {
+        
+    }
+
+    private static void QueryProduct(IProductRepository productRepository)
+    {
+        Console.WriteLine("-----------Rau có giá lớn hơn 250--------------");
+        var ProductMoreThan250 = productRepository.Find(new ProductFindCreterias()
+        {
+            MinPrice = 250
+        });
+        PrintProduct(ProductMoreThan250);
+        
+        Console.WriteLine("-----------Rau có giá nhỏ hơn 250------------");
+        var ProductLessThan250 = productRepository.Find(new ProductFindCreterias()
+        {
+            MaxPrice  = 250
+        });
+        PrintProduct(ProductLessThan250);
+        
+        Console.WriteLine("-----------Loại rau nhà rau Húng--------------");
+        var ProductNameHung = productRepository.Find(new ProductFindCreterias()
+        {
+            Name = "Húng"
+        });
+        PrintProduct(ProductNameHung);
+    }
+
+    private static void PrintProduct(IEnumerable<Product> product)
+    {
+        if (product == null)
+        {
+            Console.WriteLine("Không có sản phẩm nào");
+            return;
+        }
+        foreach (var p in product)
+        {
+            Console.WriteLine($"Id: {p.Id,20}, Name: {p.Name,20}, Price: {p.Price,20}");
+        }
+    }
+
+    private static void InsertProduct(IProductRepository productRepository)
+    {
+        productRepository.Add(new()
+        {
+            Id = new Guid("00000000-0000-0000-0000-000000000001"),
+            Name = "Rau Muống",
+            Price = 100,
+            Quantity = 100
+        });
+        productRepository.Add(new()
+        {
+            Id = new Guid("00000000-0000-0000-0000-000000000002"),
+            Name = "Rau Húng",
+            Price = 200,
+            Quantity = 200
+        });
+        productRepository.Add(new()
+        {
+            Id =new Guid("00000000-0000-0000-0000-000000000003"),
+            Name = "Rau Bí",
+            Price = 251,
+            Quantity = 300
+        });
+        productRepository.Add(new()
+        {
+            Id =new Guid("00000000-0000-0000-0000-000000000003"),
+            Name = "Rau Húng Bạc Hà",
+            Price = 400,
+            Quantity = 400
+        });
     }
 }
--- L20_21_Repository and Unit of Work/RepoAndUoW/RepoSample/obj/Debug/net8.0/RepoSample.AssemblyInfo.cs
+++ L20_21_Repository and Unit of Work/RepoAndUoW/RepoSample/obj/Debug/net8.0/RepoSample.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RepoSample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1446af72be7e337d6864aa0337d47e5b564824d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de655053a3312c6cecd34b31f614b11bc9fc6efd")]
 [assembly: System.Reflection.AssemblyProductAttribute("RepoSample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RepoSample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
