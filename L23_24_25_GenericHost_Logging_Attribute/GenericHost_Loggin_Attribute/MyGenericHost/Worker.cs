69e52b694db103fb49d0cc5ef59682b5e120970e
san san <pts.20.sn@gmail.com>
2025-05-20 10:31:17 +0700

    feat: viết và học ý thuyết về generic host, lý thuyết được ể trong file Lythuyet.txt

--- L23_24_25_GenericHost_Logging_Attribute/GenericHost_Loggin_Attribute/MyGenericHost/Worker.cs
+++ L23_24_25_GenericHost_Logging_Attribute/GenericHost_Loggin_Attribute/MyGenericHost/Worker.cs
@@ -1,24 +1,77 @@
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
 namespace MyGenericHost;
 
-public class Worker : BackgroundService
+public sealed class Worker : IHostedService, IHostedLifecycleService
 {
-    private readonly ILogger<Worker> _logger;
+    private readonly ILogger _logger;
 
-    public Worker(ILogger<Worker> logger)
+    public Worker(
+        ILogger<Worker> logger,
+        IHostApplicationLifetime appLifetime)
     {
         _logger = logger;
+
+        appLifetime.ApplicationStarted.Register(OnStarted);
+        appLifetime.ApplicationStopping.Register(OnStopping);
+        appLifetime.ApplicationStopped.Register(OnStopped);
     }
 
-    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    Task IHostedLifecycleService.StartingAsync(CancellationToken cancellationToken)
     {
-        while (!stoppingToken.IsCancellationRequested)
-        {
-            if (_logger.IsEnabled(LogLevel.Information))
-            {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-            }
+        _logger.LogInformation("1. StartingAsync has been called.");
 
-            await Task.Delay(1000, stoppingToken);
-        }
+        return Task.CompletedTask;
+    }
+
+    Task IHostedService.StartAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("2. StartAsync has been called.");
+
+        return Task.CompletedTask;
+    }
+
+    Task IHostedLifecycleService.StartedAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("3. StartedAsync has been called.");
+
+        return Task.CompletedTask;
+    }
+
+    private void OnStarted()
+    {
+        _logger.LogInformation("4. OnStarted has been called.");
+    }
+
+    private void OnStopping()
+    {
+        _logger.LogInformation("5. OnStopping has been called.");
+    }
+
+    Task IHostedLifecycleService.StoppingAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("6. StoppingAsync has been called.");
+
+        return Task.CompletedTask;
+    }
+
+    Task IHostedService.StopAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("7. StopAsync has been called.");
+
+        return Task.CompletedTask;
+    }
+
+    Task IHostedLifecycleService.StoppedAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("8. StoppedAsync has been called.");
+
+        return Task.CompletedTask;
+    }
+
+    private void OnStopped()
+    {
+        _logger.LogInformation("9. OnStopped has been called.");
     }
 }
