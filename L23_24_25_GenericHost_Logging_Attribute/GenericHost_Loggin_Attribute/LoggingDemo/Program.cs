b59da23d4a993fda1e527e188a30a6b35e3e7e28
san san <pts.20.sn@gmail.com>
2025-05-21 10:58:33 +0700

    Add: add project Loggin

---
+++ L23_24_25_GenericHost_Logging_Attribute/GenericHost_Loggin_Attribute/LoggingDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+using Microsoft.Extensions.Logging;
+
+namespace LoggingDemo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        using ILoggerFactory loggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder.AddConsole();
+        });
+        ILogger logger = loggerFactory.CreateLogger("Program");
+        logger.LogInformation("test logging info  từ console");
+    }
+}
