02cb26e9426d44096f0d3d902745b8240f8cfa3f
san san <pts.20.sn@gmail.com>
2025-05-21 16:54:17 +0700

    Finish: finish L24 logging

--- L23_24_25_GenericHost_Logging_Attribute/GenericHost_Loggin_Attribute/LoggingDemo/Program.cs
+++ L23_24_25_GenericHost_Logging_Attribute/GenericHost_Loggin_Attribute/LoggingDemo/Program.cs
@@ -11,8 +11,19 @@ class Program
         using ILoggerFactory loggerFactory = LoggerFactory.Create(builder =>
         {
             builder.AddConsole();
+            builder.SetMinimumLevel(LogLevel.Trace);
+            builder.AddFilter((level)=>level == LogLevel.Debug || level == LogLevel.Warning, LogLevel.Information);
         });
+        
         ILogger logger = loggerFactory.CreateLogger("Program");
-        logger.LogInformation("test logging info  từ console");
+        
+        
+        string n = "con bò";
+        logger.LogTrace("test LogTrace từ console");
+        logger.LogDebug("test LogDebug  từ console");
+        logger.LogInformation("test logging info  từ console {n:X}", n);
+        logger.LogWarning("test logging info  từ console {n:X}", n);
+        
+        
     }
 }
