7d8c6fe993d6280708c2547deb68081b5b5c32cb
san san <pts.20.sn@gmail.com>
2025-05-22 13:02:12 +0700

    Finish: xong bài L26 configuration

---
+++ L26_Configuration/CConfiguration/ConfigurationSample/BindingConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace ConfigurationSample;
+
+public class BindingConfig
+{
+    public string StringConfig { get; set; } = String.Empty;
+    public int IntergerConfig { get; set; } 
+    public bool BoolConfig { get; set; } 
+}
--- L26_Configuration/CConfiguration/ConfigurationSample/Program.cs
+++ L26_Configuration/CConfiguration/ConfigurationSample/Program.cs
@@ -1,10 +1,60 @@
 ﻿
+using System.Text;
+using Microsoft.Extensions.Configuration;
+
 namespace ConfigurationSample;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.OutputEncoding = Encoding.UTF8;
+        var builder = new ConfigurationBuilder().
+            AddJsonFile("ConfigFile/appsettings.json",optional: false).
+            AddJsonFile("ConfigFile/appsettings.options.json",optional: true).
+            AddXmlFile("ConfigFile/appsettings.xml",optional: false).
+            AddKeyPerFile(@"C:\Users\pts20\Desktop\.net\LearnDotNet\L26_Configuration\CConfiguration\ConfigurationSample\ConfigFile\KeyPerFile",optional: true).
+            //AddEnvironmentVariables().
+            AddCommandLine(args).
+            Build();
+        
+        
+        PrintConfigProviders(builder);
+        Console.WriteLine();
+        
+        PrintConfigValue(builder);
+        Console.WriteLine();
+        
+        var config = builder.GetRequiredSection("JsonConfigSample").Get<BindingConfig>();
+        Print(config);
+        
+        
+        
+    }
+
+    static void PrintConfigProviders(IConfigurationRoot builder)
+    {
+        foreach (var provider in builder.Providers)
+        {
+            Console.WriteLine($"providers: {provider.GetType().Name}");
+        }
+    }
+
+    static void PrintConfigValue(IConfigurationRoot builder)
+    {
+        foreach (var config in builder.AsEnumerable())
+        {
+            Console.WriteLine($"{config.Key} = {config.Value}");
+        }
+    }
+
+    static void Print(BindingConfig? config)
+    {
+        if (config != null)
+        {
+            Console.WriteLine($"-> StringConfig: {config.StringConfig}");
+            Console.WriteLine($"-> IntergerConfig: {config.IntergerConfig}");
+            Console.WriteLine($"-> BoolConfig: {config.BoolConfig}");
+        }
     }
 }
