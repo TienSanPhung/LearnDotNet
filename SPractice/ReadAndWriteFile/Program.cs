04eedcf78775a8242ea8496ad72d840df6f608ae
san san <pts.20.sn@gmail.com>
2025-05-23 16:10:24 +0700

    feat: bài 2 đọc ghi file bằng file stream theo kiểu byte

--- SPractice/ReadAndWriteFile/Program.cs
+++ SPractice/ReadAndWriteFile/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System.Runtime.InteropServices;
+using System.Text;
 using ReadAndWriteFile.Bai1;
+using ReadAndWriteFile.Bai2;
 
 namespace ReadAndWriteFile;
 
@@ -7,9 +9,11 @@ class Program
 {
     static void Main(string[] args)
     { 
-        var bai1 = new ReadWritebyStreamWriteReader(@"C:\Users\pts20\Desktop\.net\LearnDotNet\SPractice\ReadAndWriteFile\Bai1\NewFile.txt");
-        Print(bai1.ReadAndWrite());
-       
+        Console.OutputEncoding = Encoding.UTF8;
+        //var bai1 = new ReadWritebyStreamWriteReader(@"C:\Users\pts20\Desktop\.net\LearnDotNet\SPractice\ReadAndWriteFile\Bai1\NewFile.txt");
+       // Print(bai1.ReadAndWrite());
+        var bai2 = new ReadWriteByFileStream(@"C:\Users\pts20\Desktop\.net\LearnDotNet\SPractice\ReadAndWriteFile\Bai2\NewFile.txt");
+        bai2.ReadAndWrite();
     }
 
     static void Print(List<string> readAndWrite)
