4f860e41ae17c4c4495234c1d0fa66fa7cbe9a2b
san san <pts.20.sn@gmail.com>
2025-05-23 17:18:10 +0700

    feat: bìa 4 đọc ghi file theo phần bằng file.Seek

--- SPractice/ReadAndWriteFile/Program.cs
+++ SPractice/ReadAndWriteFile/Program.cs
@@ -3,6 +3,7 @@ using System.Text;
 using ReadAndWriteFile.Bai1;
 using ReadAndWriteFile.Bai2;
 using ReadAndWriteFile.Bai3;
+using ReadAndWriteFile.Bai4;
 
 namespace ReadAndWriteFile;
 
@@ -11,16 +12,19 @@ class Program
     static void Main(string[] args)
     { 
         Console.OutputEncoding = Encoding.UTF8;
-        // var bai1 = new ReadWritebyStreamWriteReader(@"C:\Users\pts20\Desktop\.net\LearnDotNet\SPractice\ReadAndWriteFile\Bai1\NewFile.txt");
-        // Print(bai1.ReadAndWrite());
-        // var bai2 = new ReadWriteByFileStream(@"C:\Users\pts20\Desktop\.net\LearnDotNet\SPractice\ReadAndWriteFile\Bai2\NewFile.txt");
-        // bai2.ReadAndWrite();
+        var bai1 = new ReadWritebyStreamWriteReader(@"C:\Users\pts20\Desktop\.net\LearnDotNet\SPractice\ReadAndWriteFile\Bai1\NewFile.txt");
+        Print(bai1.ReadAndWrite());
+        var bai2 = new ReadWriteByFileStream(@"C:\Users\pts20\Desktop\.net\LearnDotNet\SPractice\ReadAndWriteFile\Bai2\NewFile.txt");
+        bai2.ReadAndWrite();
         var bai3 = new ReadWriteByBufferedStream(@"C:\Users\pts20\Desktop\.net\LearnDotNet\SPractice\ReadAndWriteFile\Bai3\NewFile.txt");
         bai3.ReadAndWrite();
+        var bai4 = new ReadWriteByFileSeek(@"C:\Users\pts20\Desktop\.net\LearnDotNet\SPractice\ReadAndWriteFile\Bai4\NewFile.txt");
+        bai4.ReadAndWrite();
     }
 
     static void Print(List<string> readAndWrite)
     {
+        Console.WriteLine($"-------------bài 1---------------");
         foreach (var s in readAndWrite)
         {
             Console.WriteLine(s);
