85129d0cc6a2e5e718476549aaa8c4cd4e0de1d7
san san <pts.20.sn@gmail.com>
2025-05-23 10:41:39 +0700

    feat: bài 1 phần đọc ghi file - đọc ghi bằng streamwriter và streamreader

---
+++ SPractice/ReadAndWriteFile/Bai1/ReadWritebyStreamWriteReader.cs
@@ -0,0 +1,35 @@
+﻿namespace ReadAndWriteFile.Bai1;
+
+public class ReadWritebyStreamWriteReader
+{
+    readonly string filePath;
+
+    public ReadWritebyStreamWriteReader(string _filePath)
+    {
+        this.filePath = _filePath;
+    }
+    public List<string> ReadAndWrite()
+    {
+        List<string> Lines = new();
+        using (var writer = new StreamWriter(filePath)){
+            writer.WriteLine("Creates an ILoggerFactory.");
+            writer.WriteLine("The ILoggerFactory stores all the configuration that determines where log messages are sent.");
+            writer.WriteLine("In this case, you configure the console logging provider so that log messages are written to the console.");
+            writer.WriteLine("Creates an ILogger with a category named \"Program\".");
+            writer.WriteLine(" The category is a string that is associated with each message logged by the ILogger object.");
+            
+        }
+
+        using (var reader = new StreamReader(filePath))
+        {
+            string line;
+            do
+            {
+                line = reader.ReadLine();
+                if (line != null) Lines.Add(line);
+            }
+            while (line != null);
+        }
+        return Lines;
+    }
+}
--- SPractice/ReadAndWriteFile/Program.cs
+++ SPractice/ReadAndWriteFile/Program.cs
@@ -1,9 +1,22 @@
-﻿namespace ReadAndWriteFile;
+﻿using System.Runtime.InteropServices;
+using ReadAndWriteFile.Bai1;
+
+namespace ReadAndWriteFile;
 
 class Program
 {
     static void Main(string[] args)
+    { 
+        var bai1 = new ReadWritebyStreamWriteReader(@"C:\Users\pts20\Desktop\.net\LearnDotNet\SPractice\ReadAndWriteFile\Bai1\NewFile.txt");
+        Print(bai1.ReadAndWrite());
+       
+    }
+
+    static void Print(List<string> readAndWrite)
     {
-        Console.WriteLine("Hello, World!");
+        foreach (var s in readAndWrite)
+        {
+            Console.WriteLine(s);
+        }
     }
 }
